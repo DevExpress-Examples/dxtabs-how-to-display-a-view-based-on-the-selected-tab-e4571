c2c6d747d5ab32061e40e77f62b063961721f7c4
DevExpressExampleBot <fakeEmail>
2020-12-08 19:39:51 +0300

    Source auto update [skip ci]

---
+++ VB/JS/Application2/views/home.js
@@ -0,0 +1,16 @@
+﻿Application2.home = function (params) {
+    var viewModel = {
+        tabs: [
+          { text: 'Tab 0' },
+          { text: 'Tab 1' },
+          { text: 'Tab 2' }
+        ],
+        selectedTab: ko.observable(0),
+        onButtonClick: function (e) {
+            alert('Hello');
+        },
+        currentDate: ko.observable(new Date()),
+        textValue: ko.observable('test')
+    };
+    return viewModel;
+};
